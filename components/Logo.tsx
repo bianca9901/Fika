5244cf346c8420ffe7f3cfb024a9e5481e563802
bianca9901 <biancawikberg99@yahoo.com>
2024-02-09 20:38:54 +0100

    Create Logo component with framer-motion animation

---
+++ components/Logo.tsx
@@ -0,0 +1,21 @@
+// Logo.js
+import React from 'react';
+import fikaLogo from "@/public/fika-logo.png";
+import { motion } from "framer-motion";
+
+function Logo() {
+  const fikaLogoSrc = fikaLogo.src;
+
+  return (
+    <motion.img
+      src={fikaLogoSrc}
+      alt="Fika Logo"
+      className="w-20 h-auto mr-4"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    />
+  );
+}
+
+export default Logo;
