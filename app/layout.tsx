0902ca1e82ad9d997bc6062df81ae73b8cc0f752
bianca9901 <biancawikberg99@yahoo.com>
2024-02-10 14:04:15 +0100

    Update layout styles and integrate new components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import BigBulle from "@/components/BigBulleImg";
+import HeroP from "@/components/HeroP";
+import ContainerMaxWidth from "@/components/ContainerMaxWidth";
+import NavBottom from "@/components/NavBottom";
+import NavTop from "@/components/NavTop";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,10 +21,28 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
+        className={`${inter.className} bg-gray-50  relative
+         h-[5000] pt-28 sm:pt-36`}
       >
-        <div className="bg-pink-300 absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-pink-600"></div>
-        <div className="bg-pink-200 absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-pink-500"></div>
+        <div
+          className="bg-blue-100 absolute top-[-6rem] -z-10 right-[11rem]
+        h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"
+        ></div>
+        <div
+          className="bg-yellow-90 absolute top-[-1rem] -z-10 left-[-35rem]
+        h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem]
+        md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"
+        ></div>
+
+        <NavTop />
+        
+        <ContainerMaxWidth>
+          {children}   
+          <HeroP />
+          <BigBulle />
+        </ContainerMaxWidth>
+
+        <NavBottom />
       </body>
     </html>
   );
