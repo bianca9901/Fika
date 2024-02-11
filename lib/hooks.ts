5e47e377fff7a206c15c672e6d5f0db7d8048837
bianca9901 <biancawikberg99@yahoo.com>
2024-02-11 19:49:38 +0100

    Create custom hook for detecting scroll direction

---
+++ lib/hooks.ts
@@ -0,0 +1,28 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export const useScrollDirection = () => {
+  const [scrollDirection, setScrollDirection] = useState("none");
+
+  useEffect(() => {
+    let lastScrollPosition = window.scrollY;
+
+    const handleScroll = () => {
+      const currentScrollPosition = window.scrollY;
+      if (currentScrollPosition > lastScrollPosition) {
+        setScrollDirection("down");
+      } else {
+        setScrollDirection("up");
+      }
+      lastScrollPosition = currentScrollPosition;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return scrollDirection;
+};
