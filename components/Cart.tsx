403a22745949d97f6c81ead81b988c485d0cf75b
bianca9901 <biancawikberg99@yahoo.com>
2024-02-11 16:05:15 +0100

    Create Cart component
    Implemented Cart component with a shopping cart icon using the Iconify library.

---
+++ components/Cart.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { Icon } from "@iconify/react";
+
+export default function Cart() {
+  return (
+    <>
+      <Icon
+        icon="solar:cart-2-line-duotone"
+        className="w-10 h-auto text-blue-500 mr-4 ml-11"
+      />
+    </>
+  );
+}
