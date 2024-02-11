b21921f35feb92efcf902019f2781a983f2734f8
bianca9901 <biancawikberg99@yahoo.com>
2024-02-11 20:14:37 +0100

    Implement useScrollDirection hook for opacity adjustment

--- components/NavBottom.tsx
+++ components/NavBottom.tsx
@@ -4,13 +4,18 @@ import { motion } from "framer-motion";
 import { links } from "@/lib/data";
 import Link from "next/link";
 import { Icon } from "@iconify/react";
+import { useScrollDirection }  from '@/lib/hooks';
 
 export default function NavBottom() {
+  const scrollDirection = useScrollDirection();
+  const shouldApplyOpacity = scrollDirection === 'down';
+
   return (
     <nav
-      className="h-[4.25rem] fixed bottom-0 left-0 w-full bg-yellow-300
-    bg-opacity-80 border-t border-gray-200 rounded-t-3xl flex flex-row
-    justify-around items-center sm:hidden"
+      className={`h-[4.25rem] fixed bottom-0 left-0 w-full bg-yellow-300
+      border-t border-gray-200 rounded-t-3xl flex flex-row justify-around
+      items-center sm:hidden 
+      ${ shouldApplyOpacity ? 'bg-opacity-70' : ''}`}
     >
       <ul className="flex gap-10 text-lg font-semibold text-gray-600">
         {links.map((link) => (
