dd36743d0a57ece8a9cb60bdda306cb15a4df5ae
bianca9901 <biancawikberg99@yahoo.com>
2024-02-14 13:27:20 +0100

    Add more responsive design to ensure optimal presentation on all screens

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,15 +3,16 @@ import Logo from "./Logo";
 import Cart from "./Cart";
 
 export default function Header() {
-    return (
-      <header
-      className="z-50 fixed top-0 left-0 right-0 py-4 sm:h-[10.25rem] flex
-      justify-center bg-blue-50 bg-opacity-90">
-        <div className="flex items-center">
-          <Logo />
-          <NavTop />
-          <Cart />
-        </div>
-      </header>
-    );
-  }
+  return (
+    <header className="z-50 fixed top-0 left-0 right-0 py-4 flex
+     justify-center bg-blue-50 bg-opacity-90">
+      <div className="flex items-center w-full max-w-screen-xl px-6">
+        <Logo />
+        <div className="flex-grow" />
+        <NavTop />
+        <div className="flex-grow" />
+        <Cart />
+      </div>
+    </header>
+  );
+}
