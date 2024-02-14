6fbe19c03d33597a875c71d58b37fd5adec48f6d
bianca9901 <biancawikberg99@yahoo.com>
2024-02-14 17:14:38 +0100

    Add item count circle design to cart icon

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -1,13 +1,18 @@
 "use client";
 import { Icon } from "@iconify/react";
 
-export default function Cart() {
-  return (
-    <>
-      <Icon
-        icon="solar:cart-2-line-duotone"
-        className="w-10 h-auto text-blue-400 mr-4 ml-11"
-      />
-    </>
-  );
-}
+  export default function Cart() {
+    return (
+      <div className="relative">
+        <Icon
+          icon="solar:cart-2-line-duotone"
+          className="w-10 h-auto text-blue-400 mr-4 ml-11"
+        />
+        <div className="rounded-full bg-yellow-300 flex justify-center
+        items-center w-6 h-6 absolute bottom-0 right-0 transform
+         text-black">
+          20
+        </div>
+      </div>
+    );
+    } 
