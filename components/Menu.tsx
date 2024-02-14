64395ffccae5dd9bd97d0525e60624029fb301bb
bianca9901 <biancawikberg99@yahoo.com>
2024-02-14 18:24:35 +0100

    Implement conditional rendering for CartBtnGroup based on item quantity

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -5,24 +5,39 @@ import AddToCartBtn from "./AddToCartBtn";
 import { motion, useScroll, useTransform } from "framer-motion";
 import { useRef } from "react";
 import { formatCurrency } from "@/lib/formatCurrency";
+import CartBtnGroup from "./CartBtnGroup";
 
-function Menu() {
-  const ref = useRef<HTMLDivElement>(null)
+export default function Menu() {
+  const ref = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
     target: ref,
     offset: ["0 1", "1.33 1"],
-  })
+  });
 
-  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1])
- 
+  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+
+  const quantity = 2;
+
+  const decreaseCartQuantity = (id: number) => {
+    
+  };
+
+  const increaseCartQuantity = (id: number) => {
+    
+  };
+
+  const removeFromCart = (id: number) => {
+    
+  };
 
   return (
-    <motion.div ref={ref}
-    style={{
-      scale: scaleProgress,
-    }}
-      className="py-4 sm:py-6 px-6 w-full max-w-screen-xl flex flex-wrap
-    gap-10 justify-center"
+    <motion.div
+      ref={ref}
+      style={{
+        scale: scaleProgress,
+      }}
+      className="py-4 sm:py-6 px-6 w-full max-w-screen-xl flex flex-wrap gap-10
+      justify-center"
     >
       {fika.map((item) => (
         <div
@@ -31,7 +46,8 @@ function Menu() {
         >
           <h2 className="text-xl font-bold mb-2 uppercase">{item.title}</h2>
           <p className="text-gray-600 mb-2">{item.description}</p>
-          <p className="text-blue-400 font-bold">{formatCurrency(item.price)}</p>
+          <p className="text-blue-400 font-bold">{formatCurrency(item.price)}
+          </p>
           <div className="flex mb-2">
             {item.tags.map((tag) => (
               <span
@@ -43,10 +59,8 @@ function Menu() {
               </span>
             ))}
           </div>
-          <div
-            className="relative w-40 h-40 mx-auto mb-4 overflow-hidden
-          rounded-full shadow-image"
-          >
+          <div className="relative w-40 h-40 mx-auto mb-4 overflow-hidden
+          rounded-full shadow-image">
             <Image
               src={item.image}
               alt={item.title}
@@ -55,11 +69,18 @@ function Menu() {
               sizes="(max-width: 640px) 100vw, 400px"
             />
           </div>
-          <AddToCartBtn />
+          {quantity === 0 ? (
+            <AddToCartBtn />
+          ) : (
+            <CartBtnGroup 
+              quantity={quantity}
+              decreaseCartQuantity={() => decreaseCartQuantity(item.id)}
+              increaseCartQuantity={() => increaseCartQuantity(item.id)}
+              removeFromCart={() => removeFromCart(item.id)}
+            />
+          )}
         </div>
       ))}
     </motion.div>
   );
 }
-
-export default Menu;
