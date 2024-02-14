59c8186375735eaeb77e1bc046d1a5b8d7669cb6
bianca9901 <biancawikberg99@yahoo.com>
2024-02-14 13:43:35 +0100

    Update styling of AddToCartBtn

--- components/AddToCartBtn.tsx
+++ components/AddToCartBtn.tsx
@@ -6,11 +6,11 @@ function AddToCartBtn() {
   return (
     <div className="flex justify-center">
       <motion.button
-        whileHover={{ scale: 1.05, backgroundColor: '#34D399' }}
+        whileHover={{ scale: 1.05, backgroundColor: '#FFD700' }}
         whileTap={{ scale: 0.95 }}
-        className="bg-green-400 focus:outline-none focus:ring-2
-       focus:ring-green-500 focus:ring-opacity-50 text-white py-1 px-6
-       rounded-full shadow-md"
+        className="bg-yellow-300 focus:outline-none focus:ring-2
+        focus:ring-yellow-400 focus:ring-opacity-50 text-black py-1 px-6
+        rounded shadow-md"
       >
         ADD TO CART +
       </motion.button>
