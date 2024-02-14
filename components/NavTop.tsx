46a4ca5c7216e1eca260d922af478b6ac0115ba5
bianca9901 <biancawikberg99@yahoo.com>
2024-02-14 13:54:09 +0100

    Update styling of NavTop

--- components/NavTop.tsx
+++ components/NavTop.tsx
@@ -6,13 +6,13 @@ import Link from "next/link";
 
 export default function NavTop() {
   return (
-    <nav className="hidden sm:flex gap-10 text-xl font-semibold text-gray-600 list-none">
+    <nav className="hidden sm:flex gap-10 text-black list-none">
         {links.map((link) => (
           <motion.li className="relative" key={link.hash}>
             <Link href={link.hash}>
               {link.name}
               <motion.span
-                className="absolute bottom-0 left-0 w-full h-1 bg-blue-300
+                className="absolute bottom-0 left-0 w-full h-1 bg-yellow-300
                 rounded-full origin-left"
                 initial={{ scale: 0 }}
                 animate={{ scale: 1 }}
