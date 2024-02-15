da46ccd2040dec36711db2a9866d6f14715f8cfd
bianca9901 <biancawikberg99@yahoo.com>
2024-02-15 11:27:15 +0100

    Add onClick prop for button click handling

--- components/AddToCartBtn.tsx
+++ components/AddToCartBtn.tsx
@@ -1,16 +1,21 @@
-"use client"
-import React from 'react';
-import { motion } from 'framer-motion';
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
 
-function AddToCartBtn() {
+type AddToCartBtnProps = {
+  onClick: () => void;
+}
+
+function AddToCartBtn({ onClick }: AddToCartBtnProps) {
   return (
     <div className="flex justify-center">
       <motion.button
-        whileHover={{ scale: 1.05, backgroundColor: '#FFD700' }}
+        whileHover={{ scale: 1.05, backgroundColor: "#FFD700" }}
         whileTap={{ scale: 0.95 }}
         className="bg-yellow-300 focus:outline-none focus:ring-2
-        focus:ring-yellow-400 focus:ring-opacity-50 text-black py-1 px-6
-        rounded shadow-md"
+         focus:ring-yellow-400 focus:ring-opacity-50 text-black py-1 px-6
+         rounded shadow-md"
+        onClick={onClick}
       >
         ADD TO CART +
       </motion.button>
