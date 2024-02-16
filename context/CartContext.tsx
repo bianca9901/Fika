7e83627a096814a0ea4dacca40aee32a5733b72c
bianca9901 <biancawikberg99@yahoo.com>
2024-02-16 10:06:34 +0100

    Create CartContext
    Implemented CartContext to manage cart state and CartProvider to provide the cart state throughout the application

---
+++ context/CartContext.tsx
@@ -0,0 +1,72 @@
+"use client";
+import React, { createContext, useContext, useState, ReactNode } from "react";
+
+type CartItem = {
+  id: number;
+  quantity: number;
+};
+
+type CartContextType = {
+  cartItems: CartItem[];
+  addToCart: (item: CartItem) => void;
+  removeFromCart: (id: number) => void;
+  increaseQuantity: (id: number) => void;
+  decreaseQuantity: (id: number) => void;
+};
+
+const CartContext = createContext<CartContextType>({
+  cartItems: [],
+  addToCart: () => {},
+  removeFromCart: () => {},
+  increaseQuantity: () => {},
+  decreaseQuantity: () => {},
+});
+
+export const useCart = () => useContext(CartContext);
+
+type CartProviderProps = {
+  children: ReactNode;
+};
+
+export const CartProvider: React.FC<CartProviderProps> = ({ children }) => {
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  const addToCart = (item: CartItem) => {
+    setCartItems([...cartItems, item]);
+  };
+
+  const removeFromCart = (id: number) => {
+    setCartItems(cartItems.filter((item) => item.id !== id));
+  };
+
+  const increaseQuantity = (id: number) => {
+    setCartItems(
+      cartItems.map((item) =>
+        item.id === id ? { ...item, quantity: item.quantity + 1 } : item
+      )
+    );
+  };
+
+  const decreaseQuantity = (id: number) => {
+    setCartItems(
+      cartItems.map((item) =>
+        item.id === id && item.quantity > 0
+          ? { ...item, quantity: item.quantity - 1 } : item
+      )
+    );
+  };  
+
+  return (
+    <CartContext.Provider
+      value={{
+        cartItems,
+        addToCart,
+        removeFromCart,
+        increaseQuantity,
+        decreaseQuantity,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
