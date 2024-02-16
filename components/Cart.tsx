f7114f043f7ddf7ea8368a2af5345b9f6c818898
bianca9901 <biancawikberg99@yahoo.com>
2024-02-16 11:13:12 +0100

    Add functionality to calculate the total quantity of items in the cart

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -1,18 +1,27 @@
 "use client";
 import { Icon } from "@iconify/react";
+import { useCart } from "@/context/CartContext";
 
-  export default function Cart() {
-    return (
-      <div className="relative">
-        <Icon
-          icon="solar:cart-2-line-duotone"
-          className="w-10 h-auto text-blue-400 mr-4 ml-11"
-        />
-        <div className="rounded-full bg-yellow-300 flex justify-center
-        items-center w-6 h-6 absolute bottom-0 right-0 transform
-         text-black">
-          20
-        </div>
+export default function Cart() {
+  const { cartItems } = useCart();
+
+  const totalQuantity = cartItems.reduce(
+    (total, currentItem) => total + currentItem.quantity,
+    0
+  );
+
+  return (
+    <div className="relative">
+      <Icon
+        icon="solar:cart-2-line-duotone"
+        className="w-10 h-auto text-blue-400 mr-4 ml-11"
+      />
+      <div
+        className="rounded-full bg-yellow-300 flex justify-center
+      items-center w-6 h-6 absolute bottom-0 right-0 transform text-black"
+      >
+        {totalQuantity}
       </div>
-    );
-    } 
+    </div>
+  );
+}
