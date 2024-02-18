af9e8b9e78ff34a01ca4e1ebf5ffb3b041ce4515
bianca9901 <biancawikberg99@yahoo.com>
2024-02-18 08:54:02 +0100

    Create useClickOutsideToggle
    This hook provides functionality to toggle a components visibility when clicked outside of it. It uses useRef, useState, and useEffect to manage the state of the component and detect clicks outside of it. This hook can be reused across components

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 export const useScrollDirection = () => {
   const [scrollDirection, setScrollDirection] = useState("none");
@@ -26,3 +26,27 @@ export const useScrollDirection = () => {
 
   return scrollDirection;
 };
+
+
+
+export const useClickOutsideToggle = () => {
+  const [expanded, setExpanded] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        setExpanded(false);
+      }
+    };
+
+    document.addEventListener("mouseup", handleClickOutside);
+    return () => {
+      document.removeEventListener("mouseup", handleClickOutside);
+    };
+  }, [ref]);
+
+  return { expanded, setExpanded, ref };
+};
+
+export default useClickOutsideToggle;
