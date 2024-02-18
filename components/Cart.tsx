91d180a0b7bd8ad866fe85dfd1e77b9a27df54e4
bianca9901 <biancawikberg99@yahoo.com>
2024-02-18 09:41:12 +0100

    Implement logic for cart view that appears on clicking the cart icon
    Add close button functionality and implemented click outside functionality to toggle the visibility of the side cart view

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -1,20 +1,33 @@
 "use client";
 import { Icon } from "@iconify/react";
 import { useCart } from "@/context/CartContext";
+import ShoppingCart from "./ShoppingCart";
+import { useState } from "react";
 
 export default function Cart() {
   const { cartItems } = useCart();
+  const [isCartOpen, setIsCartOpen] = useState(false);
 
   const totalQuantity = cartItems.reduce(
     (total, currentItem) => total + currentItem.quantity,
     0
   );
 
+  const toggleCart = () => {
+    setIsCartOpen(!isCartOpen);
+  };
+
+  const handleCloseCart = () => {
+    setIsCartOpen(false);
+  };
+
+
   return (
     <div className="relative">
       <Icon
         icon="solar:cart-2-line-duotone"
         className="w-10 h-auto text-blue-400 mr-4 ml-11"
+        onClick={toggleCart}
       />
       <div
         className="rounded-full bg-yellow-300 flex justify-center
@@ -22,6 +35,7 @@ export default function Cart() {
       >
         {totalQuantity}
       </div>
+      {isCartOpen && <ShoppingCart onClose={handleCloseCart} />}
     </div>
   );
 }
--- components/ShoppingCart.tsx
+++ components/ShoppingCart.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-function ShoppingCart() {
+function ShoppingCart({ onClose }) {
   return (
     <div className="fixed inset-0 z-50 flex justify-end items-start">
       <div className="w-96 bg-white h-full overflow-y-auto p-4 rounded-l-lg
@@ -8,6 +8,7 @@ function ShoppingCart() {
         <div className="flex justify-between items-center mb-4">
           <h2 className="text-xl font-bold">Shopping Cart</h2>
           <button
+            onClick={onClose}
             className="text-gray-500 hover:text-gray-800 focus:outline-none"
           >
             Close
@@ -16,9 +17,7 @@ function ShoppingCart() {
         <div className="border-gray-300 mb-4"></div>
         <p>Items in cart will go here</p>
       </div>
-      <div
-        className="fixed inset-0 z-40"
-      ></div>
+      <div className="fixed inset-0 z-40" onClick={onClose}></div>
     </div>
   );
 }
