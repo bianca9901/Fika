1e2fc9fa3e98fb844417927ee658e3f9768cb225
bianca9901 <biancawikberg99@yahoo.com>
2024-02-19 09:34:00 +0100

    Add useLocalStorage custom hook

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -1,7 +1,7 @@
 "use client";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 
-export const useScrollDirection = () => {
+export function useScrollDirection () {
   const [scrollDirection, setScrollDirection] = useState("none");
 
   useEffect(() => {
@@ -27,3 +27,22 @@ export const useScrollDirection = () => {
   return scrollDirection;
 };
 
+
+export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
+  const [value, setValue] = useState<T>(() => {
+    const jsonValue = localStorage.getItem(key)
+    if (jsonValue != null) return JSON.parse(jsonValue)
+
+    if (typeof initialValue === "function") {
+      return (initialValue as () => T)()
+    } else {
+      return initialValue
+    }
+  })
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value))
+  }, [key, value])
+
+  return [value, setValue] as [typeof value, typeof setValue]
+}
