fce5c68291dd43fb3342d2c3e945a053cc2f703a
bianca9901 <biancawikberg99@yahoo.com>
2024-02-19 18:44:59 +0100

    Create About component

---
+++ components/About.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import { profileImages } from '../lib/data';
+import SectionHeading from './SectionHeading';
+
+function About() {
+  return (
+    <>
+    <SectionHeading headingText1="Only " headingText2="true Fika lovers work here" />
+    <div className="flex items-center justify-center space-x-4 flex-wrap">
+      {profileImages.map((profileImage, index) => (
+        <Image
+          key={index}
+          src={profileImage}
+          alt={`Profile image ${index + 1} of workers at Fika`}
+          width={192}
+          height={192}
+          quality={95}
+          className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-yellow-2 shadow-xl"
+        />
+      ))}
+      
+    </div>
+    <div className='flex items-center justify-center mt-10 mb-10'>
+    <p>Lorem ipsum dolor sit aentium amet tempore culpa saepe dolorum dignissimos Lorem, ipsum dolor sit
+      amet consectetur adipisicing elit. Velit in laboriosam explicabo repellat sunt accusantium laborum
+      voluptatibus, odio harum, reiciendis hic, labore autem earum veniam modi repudiandae vitae dolorum!
+      Perspiciatis..</p>
+      </div>
+    </>
+  );
+}
+
+export default About;
