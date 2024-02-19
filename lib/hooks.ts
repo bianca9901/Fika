0d0fd09b847e1133ea3dd60b7ffebd14d9a11804
bianca9901 <biancawikberg99@yahoo.com>
2024-02-19 09:31:25 +0100

    Remove useClickOutsideToggle
    Not needed for this project.

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -27,26 +27,3 @@ export const useScrollDirection = () => {
   return scrollDirection;
 };
 
-
-
-export const useClickOutsideToggle = () => {
-  const [expanded, setExpanded] = useState(false);
-  const ref = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (ref.current && !ref.current.contains(event.target as Node)) {
-        setExpanded(false);
-      }
-    };
-
-    document.addEventListener("mouseup", handleClickOutside);
-    return () => {
-      document.removeEventListener("mouseup", handleClickOutside);
-    };
-  }, [ref]);
-
-  return { expanded, setExpanded, ref };
-};
-
-export default useClickOutsideToggle;
