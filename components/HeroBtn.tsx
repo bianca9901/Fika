3df38f1aeff68904df6e1db8d888cb95b05990e0
bianca9901 <biancawikberg99@yahoo.com>
2024-02-20 10:48:01 +0100

    Add a link to go to Open Times section

--- components/HeroBtn.tsx
+++ components/HeroBtn.tsx
@@ -1,18 +1,21 @@
 "use client"
 import { motion } from 'framer-motion';
+import Link from 'next/link';
 
 function HeroBtn() {
   return (
     <div className="flex justify-center">
-      <motion.button
-        whileHover={{ scale: 1.05, backgroundColor: '#FFD700' }}
-        whileTap={{ scale: 0.95 }}
-        className="bg-yellow-300 focus:outline-none focus:ring-2
-       focus:ring-yellow-400 focus:ring-opacity-50 text-black py-1 px-6
-       rounded shadow-md"
-      >
-        SEE OPEN TIMES
-      </motion.button>
+      <Link href="#open-times">
+          <motion.button
+            whileHover={{ scale: 1.05, backgroundColor: '#FFD700' }}
+            whileTap={{ scale: 0.95 }}
+            className="bg-yellow-300 focus:outline-none focus:ring-2
+           focus:ring-yellow-400 focus:ring-opacity-50 text-black py-1 px-6
+           rounded shadow-md"
+          >
+            SEE OPEN TIMES
+          </motion.button>
+      </Link>
     </div>
   );
 }
