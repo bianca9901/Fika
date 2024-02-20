802145ea210bfe24451e31e9277c0ad17a013b61
bianca9901 <biancawikberg99@yahoo.com>
2024-02-20 09:41:22 +0100

    Update padding styling

--- components/ContainerMaxWidth.tsx
+++ components/ContainerMaxWidth.tsx
@@ -2,6 +2,6 @@ import { ReactNode } from 'react';
 
 export default function ContainerMaxWidth({ children }: { children: ReactNode }) {
   return (
-    <div className=" mx-auto w-full max-w-screen-lg md:px-2">{children}</div>
+    <div className=" mx-auto w-full max-w-screen-lg px-2">{children}</div>
   );
 }
