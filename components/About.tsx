b08d19dd0d2db3e90d9ac84273117ee188dc4816
bianca9901 <biancawikberg99@yahoo.com>
2024-02-20 09:10:05 +0100

    Format code, update style, and adjust imports

--- components/About.tsx
+++ components/About.tsx
@@ -1,30 +1,34 @@
 import Image from "next/image";
-import { profileImages } from '../lib/data';
-import SectionHeading from './SectionHeading';
+import { profileImages } from "@/lib/data";
+import SectionHeading from "./SectionHeading";
 
 function About() {
   return (
     <>
-    <SectionHeading headingText1="Only " headingText2="true Fika lovers work here" />
-    <div className="flex items-center justify-center space-x-4 flex-wrap">
-      {profileImages.map((profileImage, index) => (
-        <Image
-          key={index}
-          src={profileImage}
-          alt={`Profile image ${index + 1} of workers at Fika`}
-          width={192}
-          height={192}
-          quality={95}
-          className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-yellow-2 shadow-xl"
-        />
-      ))}
-      
-    </div>
-    <div className='flex items-center justify-center mt-10 mb-10'>
-    <p>Lorem ipsum dolor sit aentium amet tempore culpa saepe dolorum dignissimos Lorem, ipsum dolor sit
-      amet consectetur adipisicing elit. Velit in laboriosam explicabo repellat sunt accusantium laborum
-      voluptatibus, odio harum, reiciendis hic, labore autem earum veniam modi repudiandae vitae dolorum!
-      Perspiciatis..</p>
+      <SectionHeading
+        headingText1="only "
+        headingText2="true fika lovers work here"
+      />
+      <div className="flex items-center justify-center space-x-4 flex-wrap">
+        {profileImages.map((profileImage, index) => (
+          <Image
+            key={index}
+            src={profileImage}
+            alt={`Profile image ${index + 1} of workers at Fika`}
+            width={192}
+            height={192}
+            quality={95}
+            className="h-24 w-24 rounded-full object-cover border-[0.35rem]
+            border-yellow-2 shadow-xl"
+          />
+        ))}
+        <p className="mt-10">
+          Lorem ipsum dolor sit aentium amet tempore culpa saepe dolorum
+          dignissimos Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Velit in laboriosam explicabo repellat sunt accusantium laborum
+          voluptatibus, odio harum, reiciendis hic, labore autem earum veniam
+          modi repudiandae vitae dolorum! Perspiciatis..
+        </p>
       </div>
     </>
   );
