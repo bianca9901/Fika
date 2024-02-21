4db0f98cf8620e4797bb56a04db76c0545a9a628
bianca9901 <biancawikberg99@yahoo.com>
2024-02-21 09:39:28 +0100

    Create hook for tracking section visibility using react-intersection-observer

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -1,5 +1,25 @@
 "use client";
 import { useEffect, useState } from "react";
+import { useActiveSectionContext } from "@/context/ActiveSectionContext";
+import { useInView } from "react-intersection-observer";
+import type { SectionName } from "./types";
+
+export function useSectionInView(sectionName: SectionName, threshold = 0.2) {
+  const { ref, inView } = useInView({
+    threshold,
+  });
+  const { setActiveSection, timeOfLastClick } = useActiveSectionContext();
+
+  useEffect(() => {
+    if (inView && Date.now() - timeOfLastClick > 1000) {
+      setActiveSection(sectionName);
+    }
+  }, [inView, setActiveSection, timeOfLastClick, sectionName]);
+
+  return {
+    ref,
+  };
+}
 
 export function useScrollDirection () {
   const [scrollDirection, setScrollDirection] = useState("none");
