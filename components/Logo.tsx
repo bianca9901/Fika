c8d97bfdaa472632750f4ddf91966a2ccba2db7b
bianca9901 <biancawikberg99@yahoo.com>
2024-02-21 10:14:40 +0100

    Add scroll-to-top functionality when clicking on Logo component

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,12 +1,17 @@
-"use client"
+"use client";
 import fikaLogo from "@/public/fika-logo.png";
 import { motion } from "framer-motion";
 
 function Logo() {
   const fikaLogoSrc = fikaLogo.src;
 
+  const handleLogoClick = () => {
+    window.scrollTo({ top: 0 });
+  };
+
   return (
     <motion.img
+      onClick={handleLogoClick}
       src={fikaLogoSrc}
       alt="Fika Logo"
       className="w-20 h-auto mr-11 ml-4"
