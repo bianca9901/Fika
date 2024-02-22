8e382eb63cb04154776388de92b0a72efff1e8ca
bianca9901 <biancawikberg99@yahoo.com>
2024-02-22 11:49:53 +0100

    Center SectionHeading on medium and larger screens

--- components/OpenTimes.tsx
+++ components/OpenTimes.tsx
@@ -4,18 +4,23 @@ import SectionHeading from "./SectionHeading";
 function OpenTimes() {
   return (
     <section id="open-times" className="scroll-mt-28">
-    <div className="bg-blue-300 p-6 rounded-lg shadow-md">
-    <SectionHeading headingText1="Open " headingText2="times"/>
-      <div className="grid md:grid-cols-2 gap-4">
-        {days.map(({ day, time }) => (
-          <div key={day} className="flex justify-between items-center p-2
-           bg-blue-400 rounded-md text-white">
-            <span className="text-lg font-semibold">{day}</span>
-            <span>{time}</span>
-          </div>
-        ))}
+      <div className="bg-blue-300 p-6 rounded-lg shadow-md">
+        <div className="md:text-center">
+          <SectionHeading headingText1="Open " headingText2="times" />
+        </div>
+        <div className="grid md:grid-cols-2 gap-4">
+          {days.map(({ day, time }) => (
+            <div
+              key={day}
+              className="flex justify-between items-center p-2
+           bg-blue-400 rounded-md text-white"
+            >
+              <span className="text-lg font-semibold">{day}</span>
+              <span>{time}</span>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
     </section>
   );
 }
