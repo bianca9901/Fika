645a4bddb50a66884f684d21b72137ceed1c7297
bianca9901 <biancawikberg99@yahoo.com>
2024-02-24 20:10:04 +0100

    Change from color "blue" to color "sky"

--- components/OpenTimes.tsx
+++ components/OpenTimes.tsx
@@ -4,7 +4,7 @@ import SectionHeading from "./SectionHeading";
 function OpenTimes() {
   return (
     <section id="open-times" className="scroll-mt-28">
-      <div className="bg-blue-300 p-6 rounded-lg shadow-md">
+      <div className="bg-sky-500 p-6 rounded-lg shadow-md">
         <div className="md:text-center">
           <SectionHeading headingText1="Open " headingText2="times" />
         </div>
@@ -13,7 +13,7 @@ function OpenTimes() {
             <div
               key={day}
               className="flex justify-between items-center p-2
-           bg-blue-400 rounded-md text-white"
+           bg-sky-400 rounded-md text-white"
             >
               <span className="text-lg font-semibold">{day}</span>
               <span>{time}</span>
