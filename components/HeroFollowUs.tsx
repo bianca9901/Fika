8cc8311ed47da864d42102d38b2c4eefb68f9386
bianca9901 <biancawikberg99@yahoo.com>
2024-02-24 21:14:54 +0100

    Remove noreferrer and add aria-label

--- components/HeroFollowUs.tsx
+++ components/HeroFollowUs.tsx
@@ -5,19 +5,19 @@ import Link from 'next/link'
 function HeroFollowUs() {
   return (
     <div>
-      <Link href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+      <Link href="https://www.instagram.com/" target="_blank" rel="noopener" aria-label="Instagram">
         <Icon
           icon="arcticons:instagram"
           className="w-8 md:w-[5rem] h-auto text-white mr-4 ml-11"
         />
       </Link>
-      <Link href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+      <Link href="https://www.facebook.com/" target="_blank" rel="noopener" aria-label="Facebook">
         <Icon
           icon="arcticons:facebook"
           className="w-8 md:w-[5rem] h-auto text-white mr-4 ml-11"
         />
       </Link>
-      <Link href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
+      <Link href="https://www.linkedin.com/" target="_blank" rel="noopener" aria-label="Linkedin">
         <Icon
           icon="arcticons:linkedin"
           className="w-8 md:w-[5rem] h-auto text-white mr-4 ml-11"
