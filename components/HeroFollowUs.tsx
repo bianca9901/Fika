beaa3f7617d7fa86fd3972824328e3e55f16bef2
bianca9901 <biancawikberg99@yahoo.com>
2024-02-24 19:24:35 +0100

    Create HeroFollowUs component to display social-media icons

---
+++ components/HeroFollowUs.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Icon } from "@iconify/react";
+import Link from 'next/link'
+
+function HeroFollowUs() {
+  return (
+    <div>
+      <Link href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+        <Icon
+          icon="arcticons:instagram"
+          className="w-8 md:w-[5rem] h-auto text-white mr-4 ml-11"
+        />
+      </Link>
+      <Link href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+        <Icon
+          icon="arcticons:facebook"
+          className="w-8 md:w-[5rem] h-auto text-white mr-4 ml-11"
+        />
+      </Link>
+      <Link href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
+        <Icon
+          icon="arcticons:linkedin"
+          className="w-8 md:w-[5rem] h-auto text-white mr-4 ml-11"
+        />
+      </Link>
+    </div>
+  );
+}
+
+export default HeroFollowUs;
