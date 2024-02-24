650e55730604404c995ae04983d24ecc2c4458d8
bianca9901 <biancawikberg99@yahoo.com>
2024-02-24 20:07:53 +0100

    Add background color and add new components

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,27 +1,24 @@
 "use client"
-import Image from "next/image";
-import heroimg from "@/public/hero-img.png";
 import HeroMessage from "./HeroMessage";
 import { useSectionInView } from "@/lib/hooks";
+import HeroImg from "./HeroImg";
+import HeroFollowUs from "./HeroFollowUs";
 
 function HeroSection() {
   const { ref } = useSectionInView("HOME")
-
+ 
   return (
     <section ref={ref} id="home">
-    <div className="relative w-full h-screen">
+      
+    <div className="relative w-full h-screen bg-sky-300">
       <div
         className="absolute top-0 left-0 w-full h-full z-10 flex items-center
       justify-center"
       >
         <HeroMessage />
+        <HeroImg/>
+        <HeroFollowUs/>
       </div>
-      <Image
-        src={heroimg}
-        alt="Coffe shop interior"
-        fill
-        className="object-cover"
-      />
     </div>
     </section>
   );
