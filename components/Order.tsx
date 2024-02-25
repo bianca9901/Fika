5bef3d8e70c9b59f516276dc624b022ccd70e047
bianca9901 <biancawikberg99@yahoo.com>
2024-02-25 19:23:39 +0100

    Create Order component for placing orders via email

---
+++ components/Order.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import React from "react";
+import OrderText from "./OrderText";
+import { motion } from "framer-motion";
+import toast from "react-hot-toast";
+import { SendEmail } from "@/actions/sendEmail";
+
+export default function Order() {
+  return (
+    <section className="mt-40 mb-40">
+      <OrderText />
+      <form
+        className="mt-10 flex flex-col"
+        action={async (formData) => {
+          const { data, error } = await SendEmail(formData);
+          if (error) {
+            toast.error(error);
+            return;
+          }
+          toast.success("Email sent successfully");
+        }}
+      >
+        <input
+          className="h-14 px-4 rounded-lg border border-black/10"
+          name="senderEmail"
+          type="email"
+          required
+          maxLength={500}
+          placeholder="Your email"
+        />
+        <textarea
+          className="h-52 my-3 rounded-lg border-black/10 p-4"
+          name="message"
+          required
+          maxLength={5000}
+          placeholder="Additional information"
+        />
+        <motion.button
+          type="submit"
+          whileHover={{ scale: 1.05, backgroundColor: "#FFD700" }}
+          whileTap={{ scale: 0.95 }}
+          className="bg-yellow-300 focus:outline-none focus:ring-2
+             focus:ring-yellow-400 focus:ring-opacity-50 text-black py-1 px-6
+             rounded shadow-md"
+        >
+          PLACE ORDER
+        </motion.button>
+      </form>
+    </section>
+  );
+}
