37bcbad38c67c26f491bde9b2631d1016e9eeb28
bianca9901 <biancawikberg99@yahoo.com>
2024-02-25 19:08:42 +0100

    Create OrderPage page

---
+++ app/order/page.tsx
@@ -0,0 +1,13 @@
+import ContainerMaxWidth from '@/components/ContainerMaxWidth'
+import Order from '@/components/Order'
+import React from 'react'
+
+export default function OrderPage() {
+  return (
+    <main>
+      <ContainerMaxWidth>
+         <Order/>
+      </ContainerMaxWidth> 
+    </main>
+  )
+}
