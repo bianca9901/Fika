cebd5be9793f928576751e062af8a0a381587d64
bianca9901 <biancawikberg99@yahoo.com>
2024-02-25 19:32:06 +0100

    Remove handleClickOutside and add "Place Order" button
    -Removed the `useEffect` hook responsible for toggling the shoppingcart because it caused unintended behaviour with other buttons.
    
    -Added a new "Place Order" button to the shopping cart, allowing users to proceed to the order page.

--- components/ShoppingCart.tsx
+++ components/ShoppingCart.tsx
@@ -1,8 +1,9 @@
 "use client";
-import { useEffect } from "react";
 import { useCart } from "@/context/CartContext";
 import Image from "next/image";
 import { formatCurrency } from "@/lib/formatCurrency";
+import { motion } from "framer-motion";
+import Link from "next/link";
 
 type ShoppingCartProps = {
   onClose: () => void;
@@ -16,32 +17,18 @@ function ShoppingCart({ onClose }: ShoppingCartProps) {
     0
   );
 
-  useEffect(() => {
-    // Closes the shopping cart when clicking outside of it
-    function handleClickOutside(event: MouseEvent) {
-      const target = event.target as HTMLElement;
-      if (!target.closest(".shopping-cart")) {
-        onClose();
-      }
-    }
-
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [onClose]);
-
   return (
-    <div className="fixed inset-0 z-50 flex justify-end items-center
-    bg-black bg-opacity-50">
-      <div className="absolute right-0 top-0 h-full w-full max-w-md p-4
-      rounded-lg shadow-lg bg-white overflow-y-auto">
+    <div
+      className="fixed inset-0 z-50 flex justify-end items-center
+    bg-black bg-opacity-50"
+    >
+      <div
+        className="absolute right-0 top-0 h-full w-full max-w-md p-4
+      rounded-lg shadow-lg bg-white overflow-y-auto"
+      >
         <div className="flex justify-between items-center mb-4">
           <h2 className="text-xl">Shopping Cart</h2>
-          <button
-            onClick={onClose}
-            className="text-slate-500"
-          >
+          <button onClick={onClose} className="text-slate-500">
             Close
           </button>
         </div>
@@ -68,9 +55,19 @@ function ShoppingCart({ onClose }: ShoppingCartProps) {
         )}
         {cartItems.length > 0 && (
           <div className="mt-4">
-            <p className="text-lg font-semibold">
-              Total Sum: {formatCurrency(totalSum)}
-            </p>
+            <p className="text-lg">Total Sum: {formatCurrency(totalSum)}</p>
+            <Link href="/order">
+              <motion.button
+                onClick={onClose}
+                whileHover={{ scale: 1.05, backgroundColor: "#FFD700" }}
+                whileTap={{ scale: 0.95 }}
+                className="bg-yellow-300 focus:outline-none focus:ring-2
+             focus:ring-yellow-400 focus:ring-opacity-50 text-black py-1 px-6
+             rounded shadow-md"
+              >
+                PLACE ORDER
+              </motion.button>
+            </Link>
           </div>
         )}
       </div>
