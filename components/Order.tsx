4c05c03ee672ab51800bf15c120011b1eeac9a9a
bianca9901 <biancawikberg99@yahoo.com>
2024-02-26 12:25:25 +0100

    Fix import misstake

--- components/Order.tsx
+++ components/Order.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import OrderText from "./OrderText";
 import { motion } from "framer-motion";
 import toast from "react-hot-toast";
-import { SendEmail } from "@/actions/sendEmail";
+import { SendEmail } from "@/actions/SendEmail";
 
 export default function Order() {
   return (
