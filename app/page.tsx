05c09d10a49fe9637a02d92822629156e520f66c
bianca9901 <biancawikberg99@yahoo.com>
2024-02-26 18:19:55 +0100

    Create CustomShapeDividerBottom and add to page

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,13 @@ import OpenTimes from "@/components/OpenTimes";
 import About from "@/components/About";
 import SectionDivider from "@/components/SectionDivider";
 import ContainerMaxWidth from "@/components/ContainerMaxWidth";
+import CustomShapeDividerBottom from "@/components/CustomShapeDividerBottom";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <CustomShapeDividerBottom/>
       <ContainerMaxWidth>
       <SectionDivider />
       <Menu />
