dc37ae35107db2d435bd7c3c383faf746bf89493
bianca9901 <biancawikberg99@yahoo.com>
2024-02-26 18:28:42 +0100

    Improve appearance of ThemeToggle on smaller screens

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -1,30 +1,30 @@
-"use client"
+"use client";
 
-import { useTheme } from '@/context/ThemeContext'
-import React from 'react'
+import { useTheme } from "@/context/ThemeContext";
+import React from "react";
 import { Icon } from "@iconify/react";
 
 export default function ThemeToggle() {
-  const { theme, toggleTheme } = useTheme()
+  const { theme, toggleTheme } = useTheme();
   return (
-    <button className='fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem]
-    bg-opacity-80 border border-white
+    <button
+      className="fixed bottom-20 md:bottom-5 right-4 bg-white md:w-[3rem]
+      md:h-[3rem] w-[2rem] h-[2rem] bg-opacity-80 border border-white
     border-opacity-40 shadow-2xl rounded-full flex items-center
-    justify-center hover:scale-[1.15] active:scale-105 transition-all'
-    onClick={toggleTheme}>
-          {
-            theme === "light" ? (
-              <Icon
-              icon="ph:sun-thin"
-              className='w-[2rem] h-[2rem] text-sky-600'
-            />        
-            ) : (
-              <Icon
-              icon="ph:moon-thin"
-              className='w-[2rem] h-[2rem] text-sky-600'
-            />     
-            )
-        }  
+    justify-center hover:scale-[1.15] active:scale-105 transition-all"
+      onClick={toggleTheme}
+    >
+      {theme === "light" ? (
+        <Icon
+          icon="ph:sun-thin"
+          className="md:w-[2rem] md:h-[2rem] w-[1rem] h-[1rem] text-sky-600"
+        />
+      ) : (
+        <Icon
+          icon="ph:moon-thin"
+          className="md:w-[2rem] md:h-[2rem] w-[1rem] h-[1rem] text-sky-600"
+        />
+      )}
     </button>
-  )
+  );
 }
