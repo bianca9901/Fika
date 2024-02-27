1d888cecae9e323591c44d2343effee1d12b2565
bianca9901 <biancawikberg99@yahoo.com>
2024-02-27 16:04:08 +0100

    Create new page OrderConfirmationPage

---
+++ app/orderConfirmation/page.tsx
@@ -0,0 +1,10 @@
+import OrderConfirmation from '@/components/OrderConfirmation'
+import React from 'react'
+
+export default function OrderConfirmationPage() {
+  return (
+    <main>
+         <OrderConfirmation/>
+    </main>
+  )
+}
