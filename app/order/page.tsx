b5792cb5f2afbb00946619a38043ba82a78b6753
bianca9901 <biancawikberg99@yahoo.com>
2024-02-27 11:16:01 +0100

    Implement mechanism to redirect to home page if cart is empty

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -1,8 +1,16 @@
+"use client"
 import ContainerMaxWidth from '@/components/ContainerMaxWidth'
 import Order from '@/components/Order'
 import React from 'react'
+import { redirect } from 'next/navigation'
+import { useCart } from '@/context/CartContext'
 
 export default function OrderPage() {
+  const { cartItems } = useCart();
+
+  if (cartItems.length === 0) {
+    redirect('/'); // Redirects to home page if cart is empty
+  }
   return (
     <main>
       <ContainerMaxWidth>
