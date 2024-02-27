c0b15425994a22a4eb1836ca089e9ac2f7bfcf7a
bianca9901 <biancawikberg99@yahoo.com>
2024-02-27 19:32:46 +0100

    Implement dynamic iconClassName prop

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -17,7 +17,7 @@ function HeroSection() {
       >
         <HeroMessage />
         <HeroImg/>
-        <SocialMediaIcons/>
+        <SocialMediaIcons iconClassName="w-8 md:w-[5rem] h-auto text-white mr-4 ml-11" />
       </div>
     </div>
     </section>
