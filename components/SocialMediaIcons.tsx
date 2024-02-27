93b5ba0c30997042d20ad396ab6a4bbe5e9c5346
bianca9901 <biancawikberg99@yahoo.com>
2024-02-27 19:31:24 +0100

    Make SocialMediaIcons component reusable by adding className prop

--- components/SocialMediaIcons.tsx
+++ components/SocialMediaIcons.tsx
@@ -1,27 +1,37 @@
-import React from 'react';
+import React from "react";
 import { Icon } from "@iconify/react";
-import Link from 'next/link'
+import Link from "next/link";
 
-function SocialMediaIcons() {
+type SocialMediaIconsProps = {
+  iconClassName: string;
+};
+
+function SocialMediaIcons({ iconClassName }: SocialMediaIconsProps) {
   return (
     <div>
-      <Link href="https://www.instagram.com/" target="_blank" rel="noopener" aria-label="Instagram">
-        <Icon
-          icon="arcticons:instagram"
-          className="w-8 md:w-[5rem] h-auto text-white mr-4 ml-11"
-        />
+      <Link
+        href="https://www.instagram.com/"
+        target="_blank"
+        rel="noopener"
+        aria-label="Instagram"
+      >
+        <Icon icon="arcticons:instagram" className={iconClassName} />
       </Link>
-      <Link href="https://www.facebook.com/" target="_blank" rel="noopener" aria-label="Facebook">
-        <Icon
-          icon="arcticons:facebook"
-          className="w-8 md:w-[5rem] h-auto text-white mr-4 ml-11"
-        />
+      <Link
+        href="https://www.facebook.com/"
+        target="_blank"
+        rel="noopener"
+        aria-label="Facebook"
+      >
+        <Icon icon="arcticons:facebook" className={iconClassName} />
       </Link>
-      <Link href="https://www.linkedin.com/" target="_blank" rel="noopener" aria-label="Linkedin">
-        <Icon
-          icon="arcticons:linkedin"
-          className="w-8 md:w-[5rem] h-auto text-white mr-4 ml-11"
-        />
+      <Link
+        href="https://www.linkedin.com/"
+        target="_blank"
+        rel="noopener"
+        aria-label="Linkedin"
+      >
+        <Icon icon="arcticons:linkedin" className={iconClassName} />
       </Link>
     </div>
   );
