385d807e58c85ba1c490619c7bf7848a3a3c5df1
bianca9901 <biancawikberg99@yahoo.com>
2024-02-28 09:44:33 +0100

    Add Contact and SectionDivider to page

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import About from "@/components/About";
 import SectionDivider from "@/components/SectionDivider";
 import ContainerMaxWidth from "@/components/ContainerMaxWidth";
 import CustomShapeDividerBottom from "@/components/CustomShapeDividerBottom";
+import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
@@ -16,6 +17,8 @@ export default function Home() {
       <Menu />
       <SectionDivider />
       <About />
+      <SectionDivider />
+      <Contact/>
       <SectionDivider />
       </ContainerMaxWidth>
       <OpenTimes />
