206dfa19cd9902f812645ddef73c2c6f0066f011
bianca9901 <biancawikberg99@yahoo.com>
2024-03-01 12:27:11 +0100

    Refactor component to use downloaded SVG file via Next.js Image
    Before, the CustomShapeDividerBottom component had embedded SVG code. Now, the component has been refactored to use a downloaded SVG file located in the public folder. This change improves maintainability and allows for better optimisation through the Next.js Image component

--- components/CustomShapeDividerBottom.tsx
+++ components/CustomShapeDividerBottom.tsx
@@ -1,33 +1,24 @@
 "use client";
 import React from "react";
 import { useTheme } from "@/context/ThemeContext";
+import shapedivider from "@/public/shape-divider.svg";
+import Image from "next/image";
 
-const CustomShapeDividerBottom: React.FC = () => {
+export default function CustomShapeDividerBottom() {
   const { theme } = useTheme();
-
-  const shapeFillColor = theme === "dark" ? "#000000" : "#38bdf8";
 
   return (
     <div className="relative">
-      <div className="absolute bottom-0 left-0 w-full transform rotate-180">
-        <div className="custom-shape-divider-bottom-1708789176">
-          <svg
-            data-name="Layer 1"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 1200 120"
-            preserveAspectRatio="none"
-            className="w-full"
-          >
-            <path
-              d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-              className="shape-fill"
-              fill={shapeFillColor}
-            ></path>
-          </svg>
-        </div>
+      <div className="absolute bottom-0 left-0 transform rotate-180">
+        {theme === "light" && (
+          <Image
+            src={shapedivider}
+            alt="Custom Shape Divider"
+            width={2000}
+            height={220}
+          />
+        )}
       </div>
     </div>
   );
-};
-
-export default CustomShapeDividerBottom;
+}
