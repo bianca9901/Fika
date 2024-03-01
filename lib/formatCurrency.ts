6a5724736d8e74c4d24a3ed66dff11c2430942a5
bianca9901 <biancawikberg99@yahoo.com>
2024-03-01 17:00:31 +0100

    Add comment to explain purpose of formatCurrency

--- lib/formatCurrency.ts
+++ lib/formatCurrency.ts
@@ -1,9 +1,13 @@
+/**
+ * formatCurrency is a reusable Intl.NumberFormat instance
+ * configured to format numbers as currency in Swedish Krona (SEK).
+ *
+ */
 const CURRENCY_FORMATTER = new Intl.NumberFormat(undefined, {
-    currency: "SEK", style: "currency"
-})
+  currency: "SEK",
+  style: "currency",
+});
 
 export const formatCurrency = (number: number) => {
-  return (
-   CURRENCY_FORMATTER.format(number)
-  )
-}
+  return CURRENCY_FORMATTER.format(number);
+};
