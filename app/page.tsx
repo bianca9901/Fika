91e7d554c88b4427d620aad30f2c72f80ce5fea4
bianca9901 <biancawikberg99@yahoo.com>
2024-03-01 09:34:56 +0100

    Add CoffeCup component to page

--- app/page.tsx
+++ app/page.tsx
@@ -6,20 +6,22 @@ import SectionDivider from "@/components/SectionDivider";
 import ContainerMaxWidth from "@/components/ContainerMaxWidth";
 import CustomShapeDividerBottom from "@/components/CustomShapeDividerBottom";
 import Contact from "@/components/Contact";
+import { CoffeCup } from "@/components/CoffeCup";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
-      <CustomShapeDividerBottom/>
+      <CustomShapeDividerBottom />
       <ContainerMaxWidth>
-      <SectionDivider />
-      <Menu />
-      <SectionDivider />
-      <About />
-      <SectionDivider />
-      <Contact/>
-      <SectionDivider />
+        <CoffeCup />
+        <SectionDivider />
+        <Menu />
+        <SectionDivider />
+        <About />
+        <SectionDivider />
+        <Contact />
+        <SectionDivider />
       </ContainerMaxWidth>
       <OpenTimes />
     </main>
