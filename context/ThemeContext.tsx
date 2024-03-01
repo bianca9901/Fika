922f45cb976fcdd7a443edb34b8721b73128ca1c
bianca9901 <biancawikberg99@yahoo.com>
2024-03-01 16:43:22 +0100

    Add comments to ThemeContext

--- context/ThemeContext.tsx
+++ context/ThemeContext.tsx
@@ -8,44 +8,47 @@ type ThemeContextProviderProps = {
   children: React.ReactNode;
 };
 
-type ThemContextType = {
+type ThemeContextType = {
   theme: Theme;
   toggleTheme: () => void;
 };
 
-const ThemeContext = createContext<ThemContextType | null>(null);
+// Create a theme context
+const ThemeContext = createContext<ThemeContextType | null>(null);
 
 export default function ThemeContextProvider({
   children,
 }: ThemeContextProviderProps) {
-  const [theme, setTheme] = useState<Theme>("light");
+  const [theme, setTheme] = useState<Theme>("light"); // State to manage the current theme
 
+  // Function to toggle between light and dark themes
   const toggleTheme = () => {
     if (theme === "light") {
       setTheme("dark");
-      window.localStorage.setItem("theme", "dark");
-      document.documentElement.classList.add("dark");
+      window.localStorage.setItem("theme", "dark"); // Save theme preference to localStorage
+      document.documentElement.classList.add("dark"); // Add 'dark' class to the root element for dark theme
     } else {
       setTheme("light");
-      window.localStorage.setItem("theme", "light");
-      document.documentElement.classList.remove("light");
+      window.localStorage.setItem("theme", "light"); // Save theme preference to localStorage
+      document.documentElement.classList.remove("dark"); // Remove 'dark' class from the root element for light theme
     }
   };
 
+  // Initialises theme based on localStorage or system preference
   useEffect(() => {
     const localTheme = window.localStorage.getItem("theme") as Theme | null;
     if (localTheme) {
       setTheme(localTheme);
-
       if (localTheme === "dark") {
-        document.documentElement.classList.add("dark");
+        document.documentElement.classList.add("dark"); // Add 'dark' class if the stored theme is dark
       }
     } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
       setTheme("dark");
-      document.documentElement.classList.add("dark");
+      document.documentElement.classList.add("dark"); // Add 'dark' class if system preference is dark
     }
   }, []);
 
+  // Provide theme context value to children components
   return (
     <ThemeContext.Provider
       value={{
@@ -64,5 +67,6 @@ export function useTheme() {
   if (context === null) {
     throw new Error("useTheme must be used within a ThemeContextProvider");
   }
+
   return context;
 }
