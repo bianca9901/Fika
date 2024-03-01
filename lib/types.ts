2370e5283c2e8a7c750125ca6087849cd664fac5
bianca9901 <biancawikberg99@yahoo.com>
2024-03-01 17:11:36 +0100

    Add comment to explain purpose of types.ts file

--- lib/types.ts
+++ lib/types.ts
@@ -1,3 +1,8 @@
+/**
+ * SectionName represents the type for section names, which are from the names
+ * of the links defined in the data file.
+ */
+
 import { links } from "./data";
 
 export type SectionName = (typeof links)[number]["name"] | null;
