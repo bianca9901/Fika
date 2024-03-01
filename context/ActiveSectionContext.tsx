4339a8203bcc6251c692cefb767357ec843da370
bianca9901 <biancawikberg99@yahoo.com>
2024-03-01 16:52:25 +0100

    Add comments to ActiveSectionContext

--- context/ActiveSectionContext.tsx
+++ context/ActiveSectionContext.tsx
@@ -4,25 +4,29 @@ import type { SectionName } from "@/lib/types";
 import { useState, createContext, useContext } from "react";
 
 type ActiveSectionContextProviderProps = {
-  children: React.ReactNode;
+  children: React.ReactNode; // Children elements that will be wrapped by the ActiveSectionContextProvider
 };
 
+// Shape of the active section context
 type ActiveSectionContextType = {
-  activeSection: SectionName;
-  setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>;
-  timeOfLastClick: number;
-  setTimeOfLastClick: React.Dispatch<React.SetStateAction<number>>;
+  activeSection: SectionName; // Represents the currently active section
+  setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>; // Function to set the active section
+  timeOfLastClick: number; // Stores the timestamp of the last click event
+  setTimeOfLastClick: React.Dispatch<React.SetStateAction<number>>; // Function to set the timestamp of the last click event
 };
 
+// Creates an ActiveSection context
 export const ActiveSectionContext =
   createContext<ActiveSectionContextType | null>(null);
 
+// ActiveSectionContextProvider component responsible for managing the active section state
 export default function ActiveSectionContextProvider({
   children,
 }: ActiveSectionContextProviderProps) {
-  const [activeSection, setActiveSection] = useState<SectionName>("HOME");
-  const [timeOfLastClick, setTimeOfLastClick] = useState(0);
+  const [activeSection, setActiveSection] = useState<SectionName>("HOME"); // State to hold the active section
+  const [timeOfLastClick, setTimeOfLastClick] = useState(0); // State to hold the timestamp of the last click event
 
+  // Provide active section context value to children components
   return (
     <ActiveSectionContext.Provider
       value={{
@@ -37,9 +41,11 @@ export default function ActiveSectionContextProvider({
   );
 }
 
+// Custom hook to access the active section context within components
 export function useActiveSectionContext() {
   const context = useContext(ActiveSectionContext);
 
+  // Throw an error if the hook is used outside of an ActiveSectionContextProvider
   if (context === null) {
     throw new Error(
       "useActiveSectionContext must be used within an ActiveSectionContextProvider"
