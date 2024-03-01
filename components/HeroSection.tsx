9bc7c12ea067b252aae9c44ce6073f5fb608fa0e
bianca9901 <biancawikberg99@yahoo.com>
2024-03-01 09:53:16 +0100

    Remove z-10 style and format document

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,28 +1,27 @@
-"use client"
+"use client";
 import HeroMessage from "./HeroMessage";
 import { useSectionInView } from "@/lib/hooks";
 import HeroImg from "./HeroImg";
 import SocialMediaIcons from "./SocialMediaIcons";
 
 function HeroSection() {
-  const { ref } = useSectionInView("HOME")
- 
+  const { ref } = useSectionInView("HOME");
+
   return (
     <section ref={ref} id="home">
-      
-    <div className="relative w-full h-screen bg-sky-300 dark:bg-black">
-      <div
-        className="absolute top-0 left-0 w-full h-full z-10 flex items-center
+      <div className="relative w-full h-screen bg-sky-300 dark:bg-black">
+        <div
+          className="absolute top-0 left-0 w-full h-full flex items-center
       justify-center"
-      >
-        <HeroMessage />
-        <HeroImg/>
-        <div>
-           <SocialMediaIcons iconClassName="w-8 md:w-[5rem] h-auto text-white mr-4 ml-11 mb-[2rem]" />
+        >
+          <HeroMessage />
+          <HeroImg />
+          <div>
+            <SocialMediaIcons iconClassName="w-8 md:w-[5rem] h-auto text-white
+            mr-4 ml-11 mb-[2rem]" />
+          </div>
         </div>
-       
       </div>
-    </div>
     </section>
   );
 }
